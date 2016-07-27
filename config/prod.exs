ee1145a651cfbb8bb0a41f27f23e91aa1afa0289
Nikko Miu <nikko.miu@nikkomiu.com>
2016-07-27 15:00:32 -0500

    Added Prod ESX back

---
+++ config/prod.exs
@@ -0,0 +1,34 @@
+use Mix.Config
+
+config :my_wedding, MyWedding.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [host: "MyWedding.nikkomiu.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json"
+
+# Logging
+config :logger, level: :info
+
+# Releases
+config :phoenix, :serve_endpoints, true
+config :my_wedding, MyWedding.Endpoint, root: "."
+
+# Secret Key Base
+config :my_wedding, MyWedding.Endpoint,
+  secret_key_base: "${SECRET_KEY_BASE}"
+
+# Configures Ueberauth OAuth Google Strategy
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: "${GOOGLE_CLIENT_ID}",
+  client_secret: "${GOOGLE_CLIENT_SECRET}"
+
+# Database
+config :my_wedding, MyWedding.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "my_wedding_prod",
+  username: "my_wedding_svc",
+  password: "${DB_PASS}",
+  hostname: "${DB_HOST}",
+  pool_size: 25
+
+# Import secrets config
+# import_config "prod.secret.exs"
