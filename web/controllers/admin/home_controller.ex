255abfcd7407295949eb14cc27694a683ffdc7e1
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-04 14:46:49 -0500

    Updated admin dashboard Fixes #25

--- web/controllers/admin/home_controller.ex
+++ web/controllers/admin/home_controller.ex
@@ -9,7 +9,25 @@ defmodule MyWedding.Admin.HomeController do
         from u in MyWedding.User,
           select: count(u.id))
 
+    post_count =
+      Repo.one(
+        from p in MyWedding.Post,
+          select: count(p.id)
+      )
+
+    photo_count =
+      Repo.one(
+        from p in MyWedding.Photo,
+          select: count(p.id)
+      )
+
+    album_count =
+      Repo.one(
+        from p in MyWedding.Album,
+          select: count(p.id)
+      )
+
     conn
-    |> render(:index, user_count: user_count)
+    |> render(:index, user_count: user_count, post_count: post_count, album_count: album_count, photo_count: photo_count)
   end
 end
