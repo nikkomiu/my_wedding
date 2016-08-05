44b29fe8940dccd9bab57707d3aceb6aebfff618
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-04 21:03:01 -0500

    Removed duplicate migration

--- priv/repo/migrations/20160805012401_add_is_public_to_albums.exs
+++
@@ -1,9 +0,0 @@
-defmodule MyWedding.Repo.Migrations.AddIsPublicToAlbums do
-  use Ecto.Migration
-
-  def change do
-    alter table(:albums) do
-      add :is_public, :boolean, default: true
-    end
-  end
-end
