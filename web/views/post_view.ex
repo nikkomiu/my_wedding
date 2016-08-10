a59238413185e87448721c9b0cc2e94689d9d9b2
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-10 15:14:32 -0500

    Added blacked out bg for showing album

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,3 +1,15 @@
 defmodule MyWedding.PostView do
   use MyWedding.Web, :view
+
+  def render("new_js.html", _) do
+    render_simplemde()
+  end
+
+  def render("edit_js.html", _) do
+    render_simplemde()
+  end
+
+  defp render_simplemde() do
+    render(MyWedding.SharedView, "_simplemde.html", element: "post_body")
+  end
 end
