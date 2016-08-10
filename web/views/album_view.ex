a3d94b786c51929d722500af1a94226ae70a95fe
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-10 15:14:32 -0500

    Added blacked out bg for showing album

--- web/views/album_view.ex
+++ web/views/album_view.ex
@@ -1,3 +1,7 @@
 defmodule MyWedding.AlbumView do
   use MyWedding.Web, :view
+
+  def render("show_js.html", _) do
+    {:safe, "<script>$('html').css('background-color', '#292929');</script>"}
+  end
 end
