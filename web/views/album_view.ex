f84bbbe8b9e8af7500e19d4c859a7e9a83697d97
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-10 15:58:06 -0500

    Added transitions for photo albums

--- web/views/album_view.ex
+++ web/views/album_view.ex
@@ -1,7 +1,3 @@
 defmodule MyWedding.AlbumView do
   use MyWedding.Web, :view
-
-  def render("show_js.html", _) do
-    {:safe, "<script>$('html').css('background-color', '#292929');</script>"}
-  end
 end
