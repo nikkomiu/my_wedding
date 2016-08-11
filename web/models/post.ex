4ded229e41204a0b31c7d56985679c19e9a23d5a
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-11 14:17:15 -0500

    Closes #22 Added cover photo to pages

--- web/models/post.ex
+++ web/models/post.ex
@@ -7,15 +7,14 @@ defmodule MyWedding.Post do
     field :order, :integer
     field :is_active, :boolean, default: false
 
+    belongs_to :photo, MyWedding.Photo
+
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:title, :body, :order, :is_active])
+    |> cast(params, [:title, :body, :order, :is_active, :photo_id])
     |> validate_required([:title, :body])
   end
 end
