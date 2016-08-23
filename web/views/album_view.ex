cf248016e6191b641190cdb74dc43dbe09830324
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-23 17:59:38 -0500

    Fixed ReCAPTCHA loading issue from moving scripts to bottom

--- web/views/album_view.ex
+++ web/views/album_view.ex
@@ -4,4 +4,12 @@ defmodule MyWedding.AlbumView do
   def render("upload_head.html", _) do
     {:safe, "<script src='https://www.google.com/recaptcha/api.js'></script>"}
   end
+
+  def render("upload_js.html", %{skip_recaptcha: skip_recaptcha}) do
+    if skip_recaptcha do
+      render MyWedding.SharedView, "_recaptcha.html"
+    else
+      {:safe, ""}
+    end
+  end
 end
