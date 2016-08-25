f02d7b80e221e2596e59fd2dad332fa13c3509a0
Nikko Miu <nikko.miu@nikkomiu.com>
2016-08-24 20:25:26 -0500

    Added option for setting DB URL

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-DB_URL="ecto://$DB_USER:$DB_PASS@$DB_HOST/$DB_BASE"
+if [ -z "$DB_URL" ]; then
+  DB_URL="ecto://$DB_USER:$DB_PASS@$DB_HOST/$DB_BASE"
+fi
 
 ./bin/my_wedding foreground
